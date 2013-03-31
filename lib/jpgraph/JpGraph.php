5c985768b8cc1cb25c0769cb4c28439a31cddf84
Loic Doubinine <ztec@riper.fr>
2013-03-31 01:26:44 -0700

    Path & enhance error reporting

--- lib/jpgraph/JpGraph.php
+++ lib/jpgraph/JpGraph.php
@@ -8,14 +8,19 @@ class JpGraph {
 
     static function load(){
         if(self::$loaded !== true){
-            include_once 'src/jpgraph.php';
+            include_once __DIR__.'/src/jpgraph.php';
             self::$loaded = true ;
         }
     }
     static function module($moduleName){
         self::load();
         if(!in_array($moduleName,self::$modules)){
-            include_once 'src/jpgraph_'.$moduleName.'.php' ;
+            $path = __DIR__.'/src/jpgraph_'.$moduleName.'.php' ;
+            if(file_exists($path)){
+                include_once $path ;
+            }else{
+                throw new ModuleNotFoundException('The JpGraphs\'s module "'.$moduleName.'" does not exist');
+            }
         }
     }
 }
