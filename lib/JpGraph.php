e82db7da6a546d3926c24c9a346226da7aa49094
Ztec <ztec@riper.fr>
2017-02-23 10:44:15 +0100

    Fix path

--- lib/JpGraph.php
+++ lib/JpGraph.php
@@ -5,14 +5,14 @@ class JpGraph {
     static  $modules = array();
     static function load(){
         if(self::$loaded !== true){
-            include_once __DIR__.'/src/jpgraph.php';
+            include_once __DIR__.'/jpgraph/src/jpgraph.php';
             self::$loaded = true ;
         }
     }
     static function module($moduleName){
         self::load();
         if(!in_array($moduleName,self::$modules)){
-            $path = __DIR__.'/src/jpgraph_'.$moduleName.'.php' ;
+            $path = __DIR__.'/jpgraph/src/jpgraph_'.$moduleName.'.php' ;
             if(file_exists($path)){
                 include_once $path ;
             }else{
