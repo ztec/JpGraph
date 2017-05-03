251d1d2258212b76e9184d55b993666f7aac435b
ColonelMoutarde <sanchezluc@gmail.com>
2017-05-03 17:45:10 +0200

    Indentation and visibility

--- lib/JpGraph.php
+++ lib/JpGraph.php
@@ -1,21 +1,25 @@
 <?php
-namespace JpGraph ;
-class JpGraph {
-    static  $loaded = false ;
-    static  $modules = array();
-    static function load(){
+namespace JpGraph;
+
+class JpGraph 
+{
+    static public $loaded = false ;
+    static public $modules = array();
+    
+    static public function load(){
         if(self::$loaded !== true){
             include_once __DIR__.'/JpGraph/src/jpgraph.php';
             self::$loaded = true ;
         }
     }
-    static function module($moduleName){
+    
+    static public function module($moduleName){
         self::load();
         if(!in_array($moduleName,self::$modules)){
             $path = __DIR__.'/JpGraph/src/jpgraph_'.$moduleName.'.php' ;
             if(file_exists($path)){
                 include_once $path ;
-            }else{
+            } else {
                 throw new \InvalidArgumentException('The JpGraphs\'s module "'.$moduleName.'" does not exist');
             }
         }
